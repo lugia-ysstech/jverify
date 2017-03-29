d627fde8cb111a78210f935a2b4255672d1fba45
zenjava <138962816@qq.com>
2017-03-29 14:20:27 +0800

    初始化工程

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+'use strict';
+module.exports = {
+  extends: "vx",
+  globals: {
+    expectDev: true,
+  },
+};
---
+++ interface/vx-mock.js
@@ -0,0 +1,10 @@
+/**
+ * Created by liguoxin on 2017/3/14.
+ * @flow
+ */
+
+declare module 'vx-mock' {
+    declare type ExportObj = {
+    };
+    declare module.exports: ExportObj;
+}
