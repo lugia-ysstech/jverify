e840898c34653f59798603fdfc1baba5055e79f2
zenjava <138962816@qq.com>
2017-03-29 15:29:44 +0800

    新增：     *   导出到index中。

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,10 @@
  * Created by liguoxin on 2017/2/10.
  * @flow
  */
+/*:: import type { ModuleMockFactory } from 'vx-mock';*/
+
+
+const mockModuleFacotry /*: ModuleMockFactory*/ = require('./MockModule');
+module.exports = {
+  mockRequire: mockModuleFacotry
+};
